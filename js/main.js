3232cf7776d94ad714d2befa973b9dfd187cde12
keanneesteban <keanneesteban7@gmail.com>
2023-09-21 19:46:08 -0500

    added JS to modals

--- js/main.js
+++ js/main.js
@@ -1,2 +1,20 @@
 const modalOpen = '[data-open]';
 const modalClose = '[data-close]';
+const isVisible = 'is-visible';
+
+const openModal = document.querySelectorAll(modalOpen);
+const closeModal = document.querySelectorAll(modalClose);
+
+// Full Site Modals "open buttons"
+for (const elm of openModal) {
+    elm.addEventListener('click', function() {
+        const modalID = this.dataset.open;
+        document.getElementById(modalID).classList.add(isVisible);
+    })
+}
+
+for (const elm of closeModal) {
+    elm.addEventListener('click', function() {
+        this.parentElement.parentElement.classList.remove(isVisible);
+    })
+}
