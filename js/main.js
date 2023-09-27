aca15bcfd518c80245e0dfdfd18c2af8bda1a1ea
keanneesteban <keanneesteban7@gmail.com>
2023-09-27 14:29:09 -0500

    finished JS initial set up

--- js/main.js
+++ js/main.js
@@ -1,6 +1,24 @@
+const theme = 'theme';
+const dataTheme = "data-theme";
+const themeTab = ".theme-tab";
+const switcherBtn = ".switcher-btn";
+const dark = "dark";
+const light = 'light';
+const open = "open";
+const active = "active";
+
 const modalOpen = '[data-open]';
 const modalClose = '[data-close]';
 const isVisible = 'is-visible';
+
+const root = document.documentElement;
+
+// theme //
+
+const toggleTheme = document.querySelector(themeTab);
+const switcher = document.querySelectorAll(switcherBtn);
+const currentTheme = localStorage.getItem(theme);
+
 
 const openModal = document.querySelectorAll(modalOpen);
 const closeModal = document.querySelectorAll(modalClose);
