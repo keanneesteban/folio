1f5f4558dcb02f8eec2fb1485620605b68eaab0f
keanneesteban <keanneesteban7@gmail.com>
2023-10-17 13:33:41 -0500

    finished marquee with CSS and JS

--- js/main.js
+++ js/main.js
@@ -137,3 +137,12 @@ document.addEventListener('keyup', (e) => {
         document.querySelector('.modal.is-visible').classList.remove(isVisible);
     }
 });
+
+const elmsDisplayed = getComputedStyle(root).getPropertyValue('--marquee-elms-displayed');
+const marqueeContent = document.querySelector('ul.marquee-content');
+
+root.style.setProperty('--marquee-elms', marqueeContent.children.length);
+
+for (let i = 0; i < elmsDisplayed; i += 1) {
+    marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
+}
